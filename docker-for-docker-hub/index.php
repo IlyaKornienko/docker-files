debb544ce12eb238e4a3adbf02b65edfd45e57be
Illia Korniienko <ilya94732@gmail.com>
2023-07-05 14:01:37 +0200

    Chenge folder add new files

--- docker-for-docker-hub/index.php
+++ docker-for-docker-hub/index.php
@@ -1,8 +1,9 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
+echo "<body style='background-color:yellow'>";
 echo "<h1>Hello from Kubernetes</h1><br>";
 echo "Server IP Address is: $ip_server", "<br><p>";
-echo "Made by <front color=blue>Illia Korniienko"
-
+echo "Made by <front color=blue>Illia Korniienko";
+echo "<h1>v.1.0.8</h1><br>"
 ?>
